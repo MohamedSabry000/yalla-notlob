9652656d5c0891014f863c7035698f693b56d9f5
nehalmohsen22 <nehalmoh27@gmail.com>
2022-05-25 02:11:02 +0200

    omniauth Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
