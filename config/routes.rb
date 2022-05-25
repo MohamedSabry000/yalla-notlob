0d1009f1fda1087af8a7dc43c9342d1e0c5cdaf1
nehalmohsen22 <nehalmoh27@gmail.com>
2022-05-25 02:09:03 +0200

    route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
