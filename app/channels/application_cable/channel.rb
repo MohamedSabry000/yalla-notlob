86936c893914ac539292a6d749be227347fc1ebf
SamarNegm <negmsama890@gmail.com>
2022-05-27 03:18:11 +0200

    oreder channer created

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,12 @@
+# app/channels/application_cable/connection.rb
 module ApplicationCable
-  class Channel < ActionCable::Channel::Base
+  class Connection < ActionCable::Connection::Base
+    rescue_from StandardError, with: :report_error
+
+    private
+
+    def report_error(e)
+      SomeExternalBugtrackingService.notify(e)
+    end
   end
 end
